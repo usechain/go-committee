8b657caf9cdb26bf2486f0d3a6eafe42640e79d9
ningxin18 <292409083@qq.com>
2019-06-03 15:00:50 +0800

    GenerateRandomVerifier

--- contract/contract/contract.go
+++ contract/contract/contract.go
@@ -132,7 +132,7 @@ func (crt *Contract) ContractTransaction(node *usedrpc.UseRPC, ks *keystore.KeyS
 	//coinbaseUM := common.AddressToBase58Address(common.HexToAddress(coinbase)).String()
 	//xAddr := common.UmAddressToAddress(coinbase)
 	//nonce, err := node.UseGetTransactionCount(common.ToHex(xAddr[:]), "latest")
-	nonce, err := node.UseGetTransactionCount(coinbase, "latest")
+	nonce, err := node.UseGetTransactionCount(coinbase, "pending")
 	log.Info("nonce(latest)", "nonce", nonce)
 	if err != nil {
 		log.Error("Get nonce failed", "error", err)
--- contract/creditNew/interface.go
+++ contract/creditNew/interface.go
@@ -144,16 +144,18 @@ func ScanCreditSystemAccount(usechain *config.Usechain, pool *core.SharePool, no
 				err = CheckUserRegisterCert([]byte(issuerVerify.Cert), hashKeyString, id.Fpr)
 				if err != nil || pubToAddr != mainAddr {
 					// random choose 2 committee to send this verifyHash transaction
+					verifyFlag := GenerateRandomVerifier(usechain, max, addrIDstring)
+					if verifyFlag {
+						verifiedData := core.VerifiedMain{
+							Addr: mainAddr,
+							RegisterID: big.NewInt(mainID.Int64()),
+							Hashkey: common.HexToHash(hashKeyString),
+							Status: big.NewInt(4),
+						}
 
-					verifiedData := core.VerifiedMain{
-						Addr: mainAddr,
-						RegisterID: big.NewInt(mainID.Int64()),
-						Hashkey: common.HexToHash(hashKeyString),
-						Status: big.NewInt(4),
+						//Confirm stat with the contract
+						pool.AddVerifiedMain(verifiedData)
 					}
-
-					//Confirm stat with the contract
-					pool.AddVerifiedMain(verifiedData)
 					return
 				}
 
@@ -355,6 +357,16 @@ func ConfirmSubAccount(usechain *config.Usechain, sub core.VerifiedSub) error {
 		return nil
 	}
 	return nil
+}
+
+func GenerateRandomVerifier( usechain *config.Usechain, max int, addrID string) bool  {
+	idset := verify.AccountVerifier(addrID, max)
+	for _, id := range idset {
+		if id == usechain.UserProfile.CommitteeID {
+			return true
+		}
+	}
+	return false
 }
 
 func sendPublickeyShared(usechain *config.Usechain, nodelist []string, A string, max int, addrID string) ([]int){
